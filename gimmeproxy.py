d8d499af8069acb02e5b66db5c78333773afbff4
Johannes Gontrum <gontrum@uni-potsdam.de>
2016-08-18 10:42:58 +0200

    rewriting proxy

---
+++ gimmeproxy.py
@@ -0,0 +1,85 @@
+import asyncio
+import json
+import re
+from time import sleep, time
+from aiohttp import ClientSession, ProxyConnector, Timeout
+import requests
+
+number_of_proxies = 30
+request_proxies = 50
+timeout = 10.0
+
+proxies = []
+new_proxies = []
+ips = set()
+
+proxy_provider_url = "http://gimmeproxy.com/api/getProxy?get=true&" +\
+                     "protocol=http&supportsHttps=true&maxCheckPeriod=3600"
+test_url = 'https://www.immobilienscout24.de'
+test_for = 'IS24'
+
+async def fetch(proxy):
+    conn = ProxyConnector(proxy=proxy)
+    with Timeout(timeout):
+        try:
+            async with ClientSession(connector=conn) as session:
+                async with session.get(test_url) as r:
+                    if r.status == 200:
+                        if re.search(test_for, str(await r.read())):
+                            return True
+        except Exception:
+            pass
+
+async def test_server(proxy):
+    t = time()
+    if not await fetch(proxy['address']):
+        return
+    proxy['time'] = time() - t
+    proxy['last_test'] = time()
+    new_proxies.append(proxy)
+
+
+def get_proxy_servers(r):
+    responses = [requests.get(proxy_provider_url).text for i in range(r)]
+
+    for resp in responses:
+        try:
+            response = json.loads(resp)
+            if response['ipPort'] in ips:
+                continue
+            ips.add(response['ipPort'])
+
+            proxies.append({
+                'port': response['port'],
+                'ip': response['ip'],
+                'ipPort': response['ipPort'],
+                'address': response['curl'],
+                'time': -1,
+                'last_test': 0
+            })
+        except Exception:
+            pass
+
+
+def test_proxy_servers():
+    loop = asyncio.get_event_loop()
+
+    tasks = []
+    for proxy in proxies:
+        task = asyncio.ensure_future(test_server(proxy))
+        tasks.append(task)
+    loop.run_until_complete(asyncio.wait(tasks))
+
+
+def sort_proxies():
+    global proxies
+    prx = sorted(new_proxies, key=lambda x: x['time'])
+    proxies = prx[:number_of_proxies]
+
+if __name__ == '__main__':
+    get_proxy_servers(request_proxies)
+    test_proxy_servers()
+    sort_proxies()
+
+    for proxy in proxies:
+        print(proxy['ipPort'])
--- parseProxyList.py
+++ parseProxyList.py
@@ -1,4 +1,4 @@
-s = """ 
+s = """
 global
         log 127.0.0.1   local0
         log 127.0.0.1   local1 notice
@@ -24,7 +24,7 @@ defaults
         maxconn 2000
         timeout connect 5000
         timeout client 50000
-        timeout server 50000 
+        timeout server 50000
         #clitimeout 50000
         #srvtimeout 50000
 
@@ -35,20 +35,19 @@ frontend rotating_proxies
 
 backend tor
   balance roundrobin """
-hostCount=1
-#print s
-f = open("/scripts/proxies1.txt")
-lines = f.readlines()
-for x in lines:	
-	x = x.strip()
-	if len(x)>0:
-		results = "\nserver srv"+str(hostCount)+" "+x.strip()+" weight 1 maxconn 100 check"
-		#print results+"\n"
-		s += results
-	hostCount+=1
-	
-#print s
+
+hostCount = 1
+
+for line in open("/scripts/proxies.txt"):
+    line = line.strip()
+    if len(line) > 0:
+        results = ("\nserver srv{hostCount} {line} " +
+                   "weight 1 maxconn 100 check").format(hostCount=hostCount,
+                                                        line=line)
+        s += results
+        hostCount += 1
+
 with open("/etc/haproxy/haproxy.cfg", "w") as text_file:
-	text_file.write(s)
+    text_file.write(s)
 with open("/scripts/haproxy.cfg", "w") as text_file:
-	text_file.write(s)
+    text_file.write(s)
