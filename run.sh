a7463b28eb9aa3b3706609a83cb41d826ce94553
Cloud User <centos@brl-proxy-01.novalocal>
2022-08-23 08:56:45 +0000

    removed unused lines from run.sh

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ do
 	python /scripts/parse_proxy_list.py
 	echo " " >> /etc/haproxy/haproxy.cfg
 	service haproxy restart
+	echo "Service restart finished"
 	iptables -D INPUT -p tcp --dport $PORT 5577 -j DROP
-	haproxy -f /etc/haproxy/haproxy.cfg -db
 	echo "Sleeping for 10 minute"
 	sleep 600
 done
